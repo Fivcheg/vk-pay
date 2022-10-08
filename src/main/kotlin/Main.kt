361ae02736acfb70be842ae8210e8c419ae1104f
Vitalii Novgorodov <fivcheg@gmail.com>
2022-10-08 21:43:37 +0300

    Added commission calculation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 fun main(args: Array<String>) {
-    val
-    println("Hello World!")
-
+    val amount = 1000000
+    val commission = 0.75
+    val minSum = 3500
+    var totalCommisson = if (amount * commission / 100 > minSum) {(amount * commission)/10000} else {minSum/100}
+    println("$totalCommisson  рублей составляет комиссия")
 }
